a0e5d1e56a65ce64221a17508f909c0287c00d72
Mahasri02 <153530070+Mahasri02@users.noreply.github.com>
2024-05-20 14:53:46 +0530

    Update question7.py

--- question7.py
+++ question7.py
@@ -9,4 +9,20 @@ e=[3]
 # use these lists to create another list which will look like the list output in line 2
 
 # use append method to do this.
+output = []
 
+# Append the lists a, b, and c to output
+output.append(a)
+output.append(b)
+
+# Append list e to list d
+d.append(e)
+
+# Append list d to list c
+c.append(d)
+
+# Finally, append the modified list c to output
+output.append(c)
+
+# The output list now looks like the required structure
+print(output)
