51ac660495fd16c7494ff145aecc0831caa5634b
Mahasri02 <153530070+Mahasri02@users.noreply.github.com>
2024-05-20 14:27:37 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,12 @@
 #write a program to find the factorial of a nummber
+def fact(n):
+  if n==1 or num==0:
+    return 1
+  else:
+    return n*fact(n-1)
+  n = int(input())
+  result = fact(n)
+  if(n==result):
+    print(f"{n} is Factorial")
+  else:
+    print(f"{n} is not a Factorial")
